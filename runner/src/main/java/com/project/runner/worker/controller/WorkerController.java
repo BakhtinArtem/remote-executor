920e1e859db17a9c0751e533c96764ca6980d04b
BakhtinArtem <66804919+BakhtinArtem@users.noreply.github.com>
2024-08-04 14:23:09 +0200

    add files uploading to nodes

--- runner/src/main/java/com/project/runner/worker/controller/WorkerController.java
+++ runner/src/main/java/com/project/runner/worker/controller/WorkerController.java
@@ -1,16 +1,37 @@
 package com.project.runner.worker.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping()
 public class WorkerController {
 
-    @GetMapping()
+    private static final String UPLOAD_DIR = "/jarDir";
+
+    @GetMapping("/hello")
     public ResponseEntity<String> getHello() {
-        return ResponseEntity.ok().body("HELLO FROM WORKER");
+        return ResponseEntity.ok().body("HELLO FROM WORKER 1.0.1");
+    }
+
+    @PostMapping("/execute-jar")
+    public ResponseEntity<String> executeJar(@RequestParam("file") MultipartFile file, @RequestParam("name") String name) {
+        try {
+            final File uploadDir = new File(UPLOAD_DIR);
+            if (!uploadDir.exists()) {
+                return ResponseEntity.internalServerError().body("Upload directory does not exists");
+            }
+
+            File destinationFile = new File(uploadDir, name);
+            file.transferTo(destinationFile);
+
+            return ResponseEntity.ok().body("File uploaded successfully");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
