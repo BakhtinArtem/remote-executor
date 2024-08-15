dd6a5265d1f6ee3273130b10118ebbdad454ffdb
BakhtinArtem <66804919+BakhtinArtem@users.noreply.github.com>
2024-08-15 14:16:14 +0200

    add needed js libraries

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,43 @@
-import { useState } from 'react'
+import React from 'react';
+import {useCallback, useState} from 'react'
+import {addEdge, Background, Controls, ReactFlow, useEdgesState, useNodesState} from "@xyflow/react";
+import { PrimeReactProvider } from 'primereact/api';
 
-function App() {
-  const [count, setCount] = useState(0)
+const initialNodes = [
+    { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+    { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
-  return (
-    <>
-    </>
-  )
+/*
+* todo: custom nodes, layout libraries
+*
+* */
+export default function App() {
+    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+    const onConnect = useCallback(
+        (params) => setEdges((eds) => addEdge(params, eds)),
+        [setEdges],
+    );
+
+    return (
+        <>
+            <PrimeReactProvider>
+                <div style={{ width: '100vw', height: '100vh' }}>
+                    <ReactFlow
+                        nodes={nodes}
+                        edges={edges}
+                        onNodesChange={onNodesChange}
+                        onEdgesChange={onEdgesChange}
+                        onConnect={onConnect}
+                    >
+                        <Controls />
+                        <Background variant="dots" gap={12} size={1} />
+                    </ReactFlow>
+                </div>
+            </PrimeReactProvider>
+        </>
+    )
 }
-
-export default App
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+// react flow
+import '@xyflow/react/dist/style.css'
+// primeflex
+import '/node_modules/primeflex/primeflex.css'
+// primeicons
+import 'primeicons/primeicons.css'
+// primereact
+import 'primereact/resources/primereact.min.css'
+import 'primereact/resources/themes/md-light-indigo/theme.css'
+
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />
+    }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
