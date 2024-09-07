d706c729945d08006bd08c30884495346d13d565
BakhtinArtem <66804919+BakhtinArtem@users.noreply.github.com>
2024-09-07 23:44:32 +0200

    add meaningful error message GraphQL

---
+++ controller/src/main/java/com/project/controller/handler/GraphQLExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.project.controller.handler;
+
+import com.project.controller.exception.CycleDetectedException;
+import com.project.controller.exception.MultiComponentDetectedException;
+import graphql.ErrorType;
+import graphql.GraphQLError;
+import graphql.schema.DataFetchingEnvironment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.stereotype.Component;
+
+/**
+ * Overriding default exception handler in order to insert custom message in GraphQL errors
+ */
+@Component
+@Slf4j
+public class GraphQLExceptionHandler extends DataFetcherExceptionResolverAdapter {
+
+    @Override
+    protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
+//        todo: two error returning
+        log.debug("Handle error: {}", ex.toString());
+        return switch (ex) {
+            case CycleDetectedException cycleEx -> buildCustomMessage(ex, ErrorType.ValidationError);
+            case MultiComponentDetectedException componentEx -> buildCustomMessage(ex, ErrorType.ValidationError);
+            default -> super.resolveToSingleError(ex, env);
+        };
+    }
+
+    private GraphQLError buildCustomMessage(Throwable ex, ErrorType errorType) {
+        return GraphQLError.newError().message(ex.getMessage()).errorType(errorType).build();
+    }
+
+
+}
